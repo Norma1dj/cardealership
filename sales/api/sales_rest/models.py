f6d96c5668e257b2b3095dd14adc06e161ac2c16
Drew Norman-Meadows <norma1dj@gmail.com>
2023-07-24 16:31:06 -0700

    added models and added sales_rest.app.SalesRestConfig to INSTALLED_APPS

--- sales/api/sales_rest/models.py
+++ sales/api/sales_rest/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
 # Create your models here.
+
+class AutomobileVO(models.Model):
+    sold = models.BooleanField(default=False)
+    vin = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.vin
+
+
+class SalesPerson(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    employee_id = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+    
+class Customer(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    address = models.TextField()
+    phone_number = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+    
+class Sale(models.Model):
+   
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    customer = models.ForeignKey(
+        Customer,
+        related_name="sales",
+        on_delete=models.CASCADE)
+
+    automobile = models.ForeignKey(
+        AutomobileVO,
+        related_name= 'sales',
+        on_delete=models.CASCADE)
+    
+    salesperson = models.ForeignKey(
+        SalesPerson,
+        related_name='sales',
+        on_delete=models.CASCADE
+        )
+
+
+

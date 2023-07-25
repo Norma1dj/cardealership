06abfe359c9a29bf697fdc6d9c453e763bbbd481
Harold Sy <harold.a.sy@gmail.com>
2023-07-25 10:05:29 -0700

    first update

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class Technician(models.Model):
+    first_name=models.CharField(max_length=50)
+    last_name=models.CharField(max_length=50)
+    employee_id=models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
+
+
+
+
+class AutomobileVO(models.Model):
+    vin=models.CharField(max_length=50)
+    sold=models.DateTimeField()
+
+    def __str__(self):
+        return self.name
+
+
+
+class Appointment(models.Model):
+    vip=models.CharField(max_length=3)
+    date_time=models.DateTimeField()
+    reason=models.TextField()
+    status=models.TextField()
+    vin=models.CharField(max_length=50)
+    customer=models.CharField(max_length=200)
+    technician=models.ForeignKey(
+        Technician,
+        related_name="appointment"
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
+
--- service/api/service_rest/views.py
+++ service/api/service_rest/views.py
@@ -1,3 +1,103 @@
+from django.views.decorators.http import require_http_methods
 from django.shortcuts import render
+from .models import Technician, AutomobileVO, Appointment
+from common.json import ModelEncoder
+from django.http import JsonResponse
+import json
 
-# Create your views here.
+
+class TechnicianEncoder(ModelEncoder):
+    tech_model=Technician
+    properties = [
+        "first_name",
+        "last_name",
+        "employee_id",
+    ]
+
+
+class AppointmentEncoder(ModelEncoder):
+    app_model=Appointment
+    properties = [
+        "date_time",
+        "reason",
+        "status",
+        "vin",
+        "customer",
+        "technician",
+    ]
+
+
+
+@require_http_methods(["GET","POST","DELETE"])
+def list_technicians(request):
+    # GET ====================================
+    if request=="GET":
+        techs=Technician.objects.all()
+        return JsonResponse(
+            {"techs":techs},
+            encoder=TechnicianEncoder,
+        )
+    # POST ===================================
+    elif request=="POST":
+        content=json.loads(request.body)
+        try:
+            employee_id=AutomobileVO.objects.get(import_href=content["employee_id"])
+            content["employee_id"]=employee_id
+        except AutomobileVO.DoesNotExist:
+            return JsonResponse(
+                employee_id,
+                encoder=TechnicianEncoder,
+                safe=False,
+            )
+    # DELETE ==================================
+    elif request=="DELETE":
+        count,_=Technician.objects.filter(id=id).delete()
+        return JsonResponse({"deleted":count>0})
+
+# ================================================================================================
+
+@require_http_methods(["GET", "POST", "DELETE", "PUT"])
+def list_appointments(request):
+    # GET =============================================
+    if request=="GET":
+        app=Appointment.objects.all()
+        return JsonResponse(
+            {"app":app}
+            encoder=AppointmentEncoder,
+        )
+    # POST ============================================
+    elif request=="POST":
+        content=json.loads(request.body)
+        try:
+            vin=AutomobileVO.objects.get(import_href=content["vin"])
+            content["vin"]=vin
+        except AutomobileVO.DoesNotExist:
+            return JsonResponse(
+                {"messge": "Invalid vin number"}
+            )
+        app=Appointment.objects.create(**content)
+        return JsonResponse(
+            vin,
+            encoder=AppointmentEncoder,
+            safe=False
+        )
+    # DELETE ===========================================
+    elif request=="DELETE":
+        count,_=Appointment.objects.filter(id=id).delete()
+        return JsonResponse({"deleted":count>0})
+    # PUT ==============================================
+    elif request=="PUT":
+        try:
+            if "vin" in content:
+                vin=Appointment.objects.get(import_href=content["vin"])
+                content["vin"]=vin
+        except Appointment.DoesNotExist:
+            return JsonResponse({"message:":"Invalid vin number"})
+        Appointment.objects.filter(id=id.update(**content))
+        return JsonResponse(
+            vin,
+            encoder=AppointmentEncoder,
+            safe=False,
+        )        
+
+

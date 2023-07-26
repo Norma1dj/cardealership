154bee8aebf792ff7e439278cb78a53d6b32232b
Drew Norman-Meadows <norma1dj@gmail.com>
2023-07-26 10:51:50 -0700

    SalesRecordForm created. still need to get it to update to sold

--- ghi/app/src/RecordSaleForm.js
+++ ghi/app/src/RecordSaleForm.js
@@ -1,24 +1,37 @@
 import React,{ useEffect, useState } from 'react';
 
 function RecordSaleForm() {
-  const [name, setVin] = useState('')
+  const [automobile, setVin] = useState('')
   const [customer, setCustomer] = useState('')
+  const [salesperson, setSalesperson] = useState('')
+  const [price, setPrice] = useState('')
   const [salespeople, setSalespeople] = useState([])
-  const [salesperson, setManufacturer] = useState('')
+  const [customers, setCustomers] = useState([])
+  const [autos, setVins] = useState([])
+  
 
-  const handleNameChange = (event) => {
+
+
+
+
+  const handleVinChange = (event) => {
     const value = event.target.value;
     setVin(value);
   };
 
-  const handleLastNameChange = (event) => {
+  const handleCustomerChange = (event) => {
     const value = event.target.value;
     setCustomer(value);
   };
   
-  const handleManufacturerChange = (event) => {
+  const handleSalespersonChange = (event) => {
     const value = event.target.value;
-    setManufacturer(value);
+    setSalesperson(value);
+  };
+
+  const handlePriceChange = (event) => {
+    const value = event.target.value;
+    setPrice(value);
   };
 
 
@@ -27,14 +40,15 @@ function RecordSaleForm() {
 
     const data = {};
 
-    data.name = name;
+    data.automobile = automobile;
     data.customer = customer;
     data.salesperson = salesperson;
+    data.price = price;
 
     
     console.log(data);
 
-    const salesUrl = `http://localhost:8100/api/sales/`;
+    const salesUrl = `http://localhost:8090/api/sales/`;
     const fetchConfig = {
       method: "post",
       body: JSON.stringify(data),
@@ -51,24 +65,42 @@ function RecordSaleForm() {
   
     setVin('');
     setCustomer('');
-    setManufacturer('');
-    setSalespeople([]);
+    setSalesperson('');
+    setPrice('');
+
         
     }
   }
 
   const fetchData = async () => {
-    const url = 'http://localhost:8100/api/salespeople/';
+    const salespeopleUrl = 'http://localhost:8090/api/salespeople/';
+    const salespeopleresponse = await fetch(salespeopleUrl);
 
-    const response = await fetch(url);
-
-    if (response.ok) {
-      const data = await response.json();
+    if (salespeopleresponse.ok) {
+      const data = await salespeopleresponse.json();
       setSalespeople(data.salespeople);
-      console.log(data.salespeople)
-      
       
     }
+
+    const customersUrl = 'http://localhost:8090/api/customers/';
+    const customersresponse = await fetch(customersUrl);
+
+    if (customersresponse.ok) {
+      const data = await customersresponse.json();
+      setCustomers(data.customers);
+      
+    }
+
+    const automobilesUrl = 'http://localhost:8100/api/automobiles/';
+    const automobilesresponse = await fetch(automobilesUrl);
+
+    if (automobilesresponse.ok) {
+      const data = await automobilesresponse.json();
+      setVins(data.autos);
+      
+    }
+
+
 }
 
 
@@ -82,37 +114,58 @@ function RecordSaleForm() {
             <div className="row">
                 <div className="offset-3 col-6">
                     <div className="shadow p-4 mt-4">
-                        <h1>Add a Model</h1>
+                        <h1>Record Sale</h1>
                         <form onSubmit={handleSubmit} id="create-presentation-form">
-                            <div className="form-floating mb-3">
-                                <input  onChange={handleNameChange} value={name}  placeholder="name" required type="text" id="name"
-                                    name="name" className="form-control"/>
-                                <label htmlFor="name">Model Name</label>
-                            </div>
-                            <div className="form-floating mb-3">
-                                <input onChange={handleLastNameChange} value={customer} placeholder="customer" required type="text" id="customer"
-                                    name="customer" className="form-control"/>
-                                <label htmlFor="customer">Picture url</label>
-                            </div>
-                        
-                            <div className="mb-3">
-                              <select onChange={handleManufacturerChange} required value={salesperson} id="salesperson" name="salesperson" className="form-select">
-                                  <option value="">Choose a manufacturer</option>
-                                  {salespeople.map((manufacturer) => {
+                      
+                         <div className="mb-3">
+                              <select onChange={handleVinChange} required value={automobile} id="automobile" name="automobile" className="form-select">
+                                  <option value="">Choose a Auto automobile</option>
+                                  {autos.map((automobile) => {
+                                    if(!automobile.sold) {
                                   return (
-                                      <option key={manufacturer.id} value={manufacturer.id}>
-                                          {manufacturer.name}
+                                      <option key={automobile.vin} value={automobile.automobile}>
+                                          {automobile.vin}
+                                      </option>
+                                  );
+                                    } else {return null;}
+                              })}
+                              </select>
+                          </div>
+                         <div className="mb-3">
+                              <select onChange={handleCustomerChange} required value={customer} id="customer" name="customer" className="form-select">
+                                  <option value="">Choose a Customer</option>
+                                  {customers.map((customer) => {
+                                  return (
+                                      <option key={customer.id} value={customer.id}>
+                                          {customer.first_name} {customer.last_name}
                                       </option>
                                   );
                               })}
                               </select>
                           </div>
+                            <div className="mb-3">
+                              <select onChange={handleSalespersonChange} required value={salesperson} id="salesperson" name="salesperson" className="form-select">
+                                  <option value="">Choose a Salesperson</option>
+                                  {salespeople.map((salesperson) => {
+                                  return (
+                                      <option key={salesperson.id} value={salesperson.id}>
+                                          {salesperson.first_name} {salesperson.last_name}
+                                      </option>
+                                  );
+                              })}
+                              </select>
+                          </div>
+                            <div className="form-floating mb-3">
+                                <input  onChange={handlePriceChange} value={price}  placeholder="price" required type="text" id="price"
+                                    name="price" className="form-control"/>
+                                <label htmlFor="name">Sold Price</label>
+                            </div>
                            
                           
                             <button className="btn bg-success btn-primary">Create</button>
                         </form>
                         <div className="alert alert-success d-none mb-0" id="success-message">
-                            A Model has been successfully created!
+                            The sale has been successfully created!
                           </div>
                     </div>
                 </div>

bcf8403852a34991c23e110be180c312e4639874
Drew Norman-Meadows <norma1dj@gmail.com>
2023-07-26 16:47:16 -0700

    ManufacturerList.js complete

--- ghi/app/src/ManufacturerList.js
+++ ghi/app/src/ManufacturerList.js
@@ -1,12 +1,54 @@
-import React from 'react';
+
+
+import React, {useEffect, useState} from 'react';
 
 function ManufacturerList() {
-  return (
-    <div>
-      {/* Your Manufacturers component content goes here */}
-      <h1>Manufacturers</h1>
-    </div>
-  );
-}
+  const [manufacturers, setManufacturer] = useState([]);
+    
+  
+  
+  useEffect(() => {
+      fetchManufacturer();
+  }, []);
 
+  const fetchManufacturer = async() => {
+      const url = 'http://localhost:8100/api/manufacturers/';
+
+      const response = await fetch(url);
+      if (response.ok) {
+          const data = await response.json();
+          setManufacturer(data.manufacturers);
+      }
+  }
+  
+  
+
+return (
+  <div>
+  <h1>List of Manufacturers</h1>
+  <table className="table table-striped">
+      <thead>
+        <tr>
+          <th>Name</th>
+          
+        </tr>
+      </thead>
+      <tbody>
+      
+       
+        {manufacturers.map(manufacturer => {
+          return (
+            <tr key={manufacturer.id}>
+              <td>{ manufacturer.name} </td>
+          
+              
+            </tr>
+                );
+})}
+
+      </tbody>
+    </table>
+    </div>
+);
+}
 export default ManufacturerList;

869697c160fb64acbfe5cc7e7b052f51aee23047
Drew Norman-Meadows <norma1dj@gmail.com>
2023-07-26 06:42:06 -0700

    CreateModelForm.js is completed

--- ghi/app/src/AddCustomerForm.js
+++ ghi/app/src/AddCustomerForm.js
@@ -1,12 +1,112 @@
-import React from 'react';
+import React, { useEffect, useState }  from 'react';
 
 function AddCustomerForm() {
+
+  const [first_name, setFirst] = useState('')
+  const [last_name, setLast] = useState('')
+  const [address, setAddress] = useState('')
+  const [phone_number, setNumber] = useState('')
+
+  const handleFirstNameChange = (event) => {
+    const value = event.target.value;
+    setFirst(value);
+  };
+
+  const handleLastNameChange = (event) => {
+    const value = event.target.value;
+    setLast(value);
+  };
+  
+  const handleAddressChange = (event) => {
+    const value = event.target.value;
+    setAddress(value);
+  };
+
+  const handlePhoneNumberChange = (event) => {
+    const value = event.target.value;
+    setNumber(value);
+  };
+
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const data = {};
+
+    data.first_name = first_name;
+    data.last_name = last_name;
+    data.address = address;
+    data.phone_number = phone_number;
+
+    
+    console.log(data);
+
+    const customerUrl = `http://localhost:8090/api/customers/`;
+    const fetchConfig = {
+      method: "post",
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+  
+    const response = await fetch(customerUrl, fetchConfig);
+    if (response.ok) {
+      const newCustomer = await response.json();
+      console.log(newCustomer);
+
+  
+    setFirst('');
+    setLast('');
+    setAddress('');
+    setNumber('');
+        
+    }
+  }
+
+  // useEffect(()  => { 
+  //   fetchData();
+  // }, []);
+
+
   return (
-    <div>
-      {/* Add your Add Customer Form content here */}
-      <h1>Add Customer Form</h1>
-    </div>
-  );
+    <div className="container">
+            <div className="row">
+                <div className="offset-3 col-6">
+                    <div className="shadow p-4 mt-4">
+                        <h1>Add Customer</h1>
+                        <form onSubmit={handleSubmit} id="create-presentation-form">
+                            <div className="form-floating mb-3">
+                                <input  onChange={handleFirstNameChange} value={first_name}  placeholder="first_name" required type="text" id="first_name"
+                                    name="first_name" className="form-control"/>
+                                <label htmlFor="first_name">First Name</label>
+                            </div>
+                            <div className="form-floating mb-3">
+                                <input onChange={handleLastNameChange} value={last_name} placeholder="last_name" required type="text" id="last_name"
+                                    name="last_name" className="form-control"/>
+                                <label htmlFor="last_name">Last Name</label>
+                            </div>
+                            <div className="form-floating mb-3">
+                                <input onChange={handleAddressChange} value={address} placeholder="address" required type="text" id="address"
+                                    name="address" className="form-control"/>
+                                <label htmlFor="address">Address</label>
+                            </div>
+                            <div className="form-floating mb-3">
+                                <input onChange={handlePhoneNumberChange} value={phone_number} placeholder="phone_number" required type="text" id="phone_number"
+                                    name="phone_number" className="form-control"/>
+                                <label htmlFor="phone_number">Phone Number</label>
+                            </div>
+                          
+                            <button className="btn bg-success btn-primary">Create</button>
+                        </form>
+                        <div className="alert alert-success d-none mb-0" id="success-message">
+                            Your Shoe has been successfully created!
+                          </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+         );
 }
 
 export default AddCustomerForm;
--- ghi/app/src/CreateModelForm.js
+++ ghi/app/src/CreateModelForm.js
@@ -1,12 +1,134 @@
-import React from 'react';
+import React,{ useEffect, useState } from 'react';
 
 function CreateModelForm() {
-  return (
-    <div>
-      {/* Add your Create Model Form content here */}
-      <h1>Create Model Form</h1>
-    </div>
-  );
+
+    const [name, setName] = useState('')
+    const [picture_url, setPicture] = useState('')
+    const [manufacturers, setManufacturers] = useState([])
+    const [manufacturer_id, setManufacturer] = useState('')
+  
+    const handleNameChange = (event) => {
+      const value = event.target.value;
+      setName(value);
+    };
+  
+    const handleLastNameChange = (event) => {
+      const value = event.target.value;
+      setPicture(value);
+    };
+    
+    const handleManufacturersChange = (event) => {
+      const value = event.target.value;
+      setManufacturers(value);
+    };
+
+    const handleManufacturerChange = (event) => {
+      const value = event.target.value;
+      setManufacturer(value);
+    };
+  
+  
+    const handleSubmit = async (event) => {
+      event.preventDefault();
+  
+      const data = {};
+  
+      data.name = name;
+      data.picture_url = picture_url;
+      data.manufacturer_id = manufacturer_id;
+  
+      
+      console.log(data);
+  
+      const modelsUrl = `http://localhost:8100/api/models/`;
+      const fetchConfig = {
+        method: "post",
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      };
+    
+      const response = await fetch(modelsUrl, fetchConfig);
+      if (response.ok) {
+        const newModel = await response.json();
+        console.log(newModel);
+  
+    
+      setName('');
+      setPicture('');
+      setManufacturer('');
+      setManufacturers([]);
+          
+      }
+    }
+
+    const fetchData = async () => {
+      const url = 'http://localhost:8100/api/manufacturers/';
+  
+      const response = await fetch(url);
+  
+      if (response.ok) {
+        const data = await response.json();
+        setManufacturers(data.manufacturers);
+        console.log(data.manufacturers)
+        
+        
+      }
+  }
+
+  
+    useEffect(()  => { 
+      fetchData();
+    }, []);
+  
+  
+    return (
+      <div className="container">
+              <div className="row">
+                  <div className="offset-3 col-6">
+                      <div className="shadow p-4 mt-4">
+                          <h1>Add a Model</h1>
+                          <form onSubmit={handleSubmit} id="create-presentation-form">
+                              <div className="form-floating mb-3">
+                                  <input  onChange={handleNameChange} value={name}  placeholder="name" required type="text" id="name"
+                                      name="name" className="form-control"/>
+                                  <label htmlFor="name">Model Name</label>
+                              </div>
+                              <div className="form-floating mb-3">
+                                  <input onChange={handleLastNameChange} value={picture_url} placeholder="picture_url" required type="text" id="picture_url"
+                                      name="picture_url" className="form-control"/>
+                                  <label htmlFor="picture_url">Picture url</label>
+                              </div>
+                              {/* <div className="form-floating mb-3">
+                                  <input onChange={handleManufacturerChange} value={manufacturer} placeholder="manufacturer" required type="text" id="manufacturer"
+                                      name="manufacturer" className="form-control"/>
+                                  <label htmlFor="manufacturer">manufa</label>
+                              </div> */}
+                              <div className="mb-3">
+                                <select onChange={handleManufacturerChange} required value={manufacturer_id} id="manufacturer_id" name="manufacturer_id" className="form-select">
+                                    <option value="">Choose a manufacturer</option>
+                                    {manufacturers.map((manufacturer) => {
+                                    return (
+                                        <option key={manufacturer.id} value={manufacturer.id}>
+                                            {manufacturer.name}
+                                        </option>
+                                    );
+                                })}
+                                </select>
+                            </div>
+                             
+                            
+                              <button className="btn bg-success btn-primary">Create</button>
+                          </form>
+                          <div className="alert alert-success d-none mb-0" id="success-message">
+                              A Model has been successfully created!
+                            </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+    );
 }
 
 export default CreateModelForm;

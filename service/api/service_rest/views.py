d0d70b514d903a2ec52d21ecdc5fb9a86170f3e0
Harold Sy <harold.a.sy@gmail.com>
2023-07-26 16:06:50 -0700

    ninth

--- service/api/service_rest/views.py
+++ service/api/service_rest/views.py
@@ -13,14 +13,14 @@ class TechnicianEncoder(ModelEncoder):
         "first_name",
         "last_name",
         "employee_id",
-        "id"                                  # added id for django primary key
+    #    "id"                                  # added id for django primary key
     ]
 
 
 class AppointmentEncoder(ModelEncoder):
     model=Appointment
     properties = [
-        "vip"
+        "vip",
         "date_time",
         "reason",
         "status",
@@ -108,24 +108,24 @@ def list_appointments(request, pk=None):
     #           appointments
     if request.method == "GET":
         try:
-            appoint=Appointment.objects.all()
+            appointment=Appointment.objects.all()
             return JsonResponse(
-                {"appoint":appoint},
+                {"appointment":appointment},
                 encoder=AppointmentEncoder,
                 safe=False,
             )
         except:
-            response=JsonResponse({"message": "Could not connect to the server"})
-            response.status_code=404
-            return response
+           response=JsonResponse({"message": "Could not connect to the server"})
+           response.status_code=404
+           return response
     # POST ============================================
     #      Note: Code to create a new appointment
     elif request.method == "POST":
         try:
             content=json.loads(request.body)
-            appoint=Appointment.objects.create(**content)
+            appointment=Appointment.objects.create(**content)
             return JsonResponse(
-                appoint,
+                appointment,
                 encoder = AppointmentEncoder,
                 safe=False,
             )
@@ -137,10 +137,10 @@ def list_appointments(request, pk=None):
     #        Note: Code to delete an existing appointment.
     elif request.method == "DELETE":
         try:
-            appoint=Appointment.objects.get(id=pk)
-            appoint.delete()
+            appointment=Appointment.objects.get(id=pk)
+            appointment.delete()
             return JsonResponse({"message":"Appointment has been deleted"})
-        except appoint.DoesNotExist:
+        except Appointment.DoesNotExist:
             response=JsonResponse({"message":"Appointment does not exist"})
         except Exception as e:
             response=JsonResponse({"message":"Could not delete this appointment"})
@@ -151,12 +151,12 @@ def list_appointments(request, pk=None):
     elif request.method == "PUT":
         try:
             if pk==None:
-                appoint=Appointment.objects.all()
+                appointment=Appointment.objects.all()
             else:
-                appoint=Appointment.objects.filter(id=pk)
+                appointment=Appointment.objects.filter(id=pk)
             Appointment.objects.filter(id=id.update(**content))
             return JsonResponse(
-                {"appoint":appoint},
+                {"appointment":appointment},
                 encoder=AppointmentEncoder,
                 safe=False,
             )
